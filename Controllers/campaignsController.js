f352d1b66dd77d46a5f22530296d795b827a6fbb
Kamal <kamal_dia@hotmail.com>
2023-11-23 20:16:59 +0200

    finished Campaigns' functionalities (needs debbugging)

--- Controllers/campaignsController.js
+++ Controllers/campaignsController.js
@@ -1,6 +1,6 @@
 import Campaign from "../Models/campaignsModel";
 
-//Post a Campaign
+//Post a Campaign ----------------------------------------------------------------------------------------------------
 const createCampaign = async (req, res) => {
   try { 
     const new_campaign = await Campaign.create(req.body);
@@ -22,7 +22,7 @@ const createCampaign = async (req, res) => {
   }
 }
 
-//Get all campaigns
+//Get all campaigns ----------------------------------------------------------------------------------------------------
 const getAllCampaigns = async (req, res) => {
   try {
     const all_campaigns = await Campaign.findAll();
@@ -54,7 +54,7 @@ const getAllCampaigns = async (req, res) => {
   }
 }
 
-//Get accepted campaigns
+//Get accepted campaigns ----------------------------------------------------------------------------------------------------
 const getAcceptedCampaigns = async (req, res) => {
   try {
     const accepted_campaigns = await Campaign.findAll({
@@ -90,7 +90,7 @@ const getAcceptedCampaigns = async (req, res) => {
   }
 }
 
-//Get pending campaigns
+//Get pending campaigns ----------------------------------------------------------------------------------------------------
 const getPendingCampaigns = async (req, res) => {
   try {
     const pending_campaigns = await Campaign.findAll({
@@ -126,7 +126,7 @@ const getPendingCampaigns = async (req, res) => {
   }
 }
 
-//Get hidden campaigns
+//Get hidden campaigns ----------------------------------------------------------------------------------------------------
 const getHiddenCampaigns = async (req, res) => {
   try {
     const hidden_campaigns = await Campaign.findAll({
@@ -162,7 +162,7 @@ const getHiddenCampaigns = async (req, res) => {
   }
 }
 
-//Get visible campaigns
+//Get visible campaigns ----------------------------------------------------------------------------------------------------
 const getVisibleCampaigns = async (req, res) => {
   try {
     const visible_campaigns = await Campaign.findAll({
@@ -198,7 +198,7 @@ const getVisibleCampaigns = async (req, res) => {
   }
 }
 
-//Get campaign by campaignName
+//Get campaign by campaignName ----------------------------------------------------------------------------------------------------
 const getCampaignByCampaignName = async (req, res) => {
   try {
     const requested_campaign_name = req.params.name; //put :name in url as parameter
@@ -235,7 +235,7 @@ const getCampaignByCampaignName = async (req, res) => {
   }
 }
 
-//Accept(update isAccepted) a campaign by name
+//Accept(update isAccepted) a campaign by name ----------------------------------------------------------------------------------------------------
 const acceptCampaign = async (req, res) => {
   try {
     const campaign_name_to_accept = req.params.name;
@@ -245,7 +245,7 @@ const acceptCampaign = async (req, res) => {
       where: {
         campaign_name: campaign_name_to_accept,
       },
-      // returning: true, //to return the updated value
+      // returning: true, //to return the updated value, uncomment the returning line and add an array as a second item in campaign rows array
     });
     if (accepted_campaign_rows > 0) {
       res.sendStatus(202) //accepted
@@ -263,19 +263,19 @@ const acceptCampaign = async (req, res) => {
   }
 }
 
-//Hide(update isHidden) a campaign by name
+//Hide(update isHidden) a campaign by name ----------------------------------------------------------------------------------------------------
 const hideCampaign = async (req, res) => {
   try {
-    const campaign_name_to_hide = req.params.name;
-    const [hidden_campaign_rows] = await Campaign.update({ //put hidden campaign in array since update() returns an array with updated row numbers
+    const campaign_name_to_hide = req.params.name; //put :name in url
+    const [number_of_campaign_changed_rows_hide] = await Campaign.update({ //put number of campaign changed rows in array since update() returns an array with updated row numbers
       isHidden: true,
     },{
       where: {
         campaign_name: campaign_name_to_hide,
       },
-      // returning: true, //to return the updated value
+      // returning: true, //to return the updated value, uncomment the returning line and add an array as a second item in number of rows array
     });
-    if (hidden_campaign_rows > 0) {
+    if (number_of_campaign_changed_rows_hide > 0) {
       res.sendStatus(202) //accepted
     } else {
       res.sendStatus(404)
@@ -291,21 +291,245 @@ const hideCampaign = async (req, res) => {
   }
 }
 
-//unhide
+//Unhide(update isHidden) a campaign by name ----------------------------------------------------------------------------------------------------
+const unhideCampaign = async (req, res) => {
+  try {
+    const campaign_name_to_hide = req.params.name; //put :name in url
+    const [number_of_campaign_changed_rows_unhide] = await Campaign.update({ //put number of campaign rows in array since update() returns an array with updated row numbers
+      isHidden: false,
+    },{
+      where: {
+        campaign_name: campaign_name_to_hide,
+      },
+      // returning: true, //to return the updated value, uncomment the returning line and add an array as a second item in number of rows array
+    });
+    if (number_of_campaign_changed_rows_unhide > 0) {
+      res.sendStatus(202) //accepted
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't unhide the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change campaign name
+//change campaign name ----------------------------------------------------------------------------------------------------
+const changeCampaignName = async (req, res) => {
+  try {
+    const campaign_name_to_change = req.params.name; //put :name in url
+    const new_campaign_name = req.params.newName; //put :newName in url
+    const [number_of_campaign_changed_rows_name,[edited_campaign_name]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      campaign_name: new_campaign_name,
+    },{
+      where: {
+        campaign_name: campaign_name_to_change,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_name > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_name.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign name successfully to: ${edited_campaign_name.toJSON().campaign_name}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change name of the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change description
+//change description ----------------------------------------------------------------------------------------------------
+const changeCampaignDescription = async (req, res) => {
+  try {
+    const input_campaign_name = req.params.name; //put :name in url
+    const new_description = req.params.description; //put :description in url
+    const [number_of_campaign_changed_rows_description,[edited_campaign_description]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      description: new_description,
+    },{
+      where: {
+        campaign_name: input_campaign_name,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_description > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_description.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign description successfully to: ${edited_campaign_description.toJSON().description}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change description for the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change target
+//change target ----------------------------------------------------------------------------------------------------
+const changeCampaignTarget = async (req, res) => {
+  try {
+    const input_campaign_name = req.params.name; //put :name in url
+    const new_target = req.params.target; //put :target in url
+    const [number_of_campaign_changed_rows_target,[edited_campaign_target]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      target: new_target,
+    },{
+      where: {
+        campaign_name: input_campaign_name,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_target > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_target.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign target successfully to: ${edited_campaign_target.toJSON().target}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change target for the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change amount
+//change amount ----------------------------------------------------------------------------------------------------
+const changeCampaignAmount = async (req, res) => {
+  try {
+    const input_campaign_name = req.params.name; //put :name in url
+    const new_amount = req.params.amount; //put :amount in url
+    const [number_of_campaign_changed_rows_amount,[edited_campaign_amount]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      amount: new_amount,
+    },{
+      where: {
+        campaign_name: input_campaign_name,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_amount > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_amount.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign amount successfully to: ${edited_campaign_amount.toJSON().amount}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change amount for the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change start date
+//change start date ----------------------------------------------------------------------------------------------------
+const changeCampaignStartDate = async (req, res) => {
+  try {
+    const input_campaign_name = req.params.name; //put :name in url
+    const new_start_date = req.params.startDate; //put :startDate in url using YYYY-MM-DD format and try it
+    const [number_of_campaign_changed_rows_start_date,[edited_campaign_start_date]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      start_date: new_start_date,
+    },{
+      where: {
+        campaign_name: input_campaign_name,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_start_date > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_start_date.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign start date successfully to: ${edited_campaign_start_date.toJSON().start_date}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change start date for the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//change end date
+//change end date ----------------------------------------------------------------------------------------------------
+const changeCampaignEndDate = async (req, res) => {
+  try {
+    const input_campaign_name = req.params.name; //put :name in url
+    const new_end_date = req.params.endDate; //put :endDate in url using YYYY-MM-DD format and try it
+    const [number_of_campaign_changed_rows_end_date,[edited_campaign_end_date]] = await Campaign.update({ //we put campaign rows in array since update() returns an array with updated row numbers, and an array of updated value as second item
+      end_date: new_end_date,
+    },{
+      where: {
+        campaign_name: input_campaign_name,
+      },
+      returning: true, //to return the updated value
+    });
+    if (number_of_campaign_changed_rows_end_date > 0) {
+      res.status(200) //ok
+      .json({
+        data: edited_campaign_end_date.toJSON(), //convert to json to see object
+        status: 200,
+        success: true,
+        message: `changed the campaign end date successfully to: ${edited_campaign_end_date.toJSON().end_date}`, //check if this works
+      });
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    return res.status(500) //internal server error
+    .json({
+      data: null,
+      status: 500,
+      success: false,
+      message: `Couldn't change end date for the chosen campaign due to server error: ${error}`,
+    });
+  }
+}
 
-//Delete a campaign by name
+//Delete a campaign by name ----------------------------------------------------------------------------------------------------
 const deleteCampaignByCampaignName = async (req, res) => {
   try {
     const campaign_name_to_delete = req.params.name; // put :name as parameter in the url
@@ -330,7 +554,7 @@ const deleteCampaignByCampaignName = async (req, res) => {
   }
 }
 
-//Delete pending campaigns
+//Delete pending campaigns ----------------------------------------------------------------------------------------------------
 const deletePendingCampaigns = async (req, res) => {
   try {
     const deleted_campaign_rows = await Campaign.destroy({
@@ -355,6 +579,8 @@ const deletePendingCampaigns = async (req, res) => {
   }
 }
 
+//export controllers ----------------------------------------------------------------------------------------------------
+
 exports = {
   createCampaign,
   getAllCampaigns,
@@ -365,6 +591,13 @@ exports = {
   getCampaignByCampaignName,
   acceptCampaign,
   hideCampaign,
+  unhideCampaign,
+  changeCampaignName,
+  changeCampaignDescription,
+  changeCampaignTarget,
+  changeCampaignAmount,
+  changeCampaignStartDate,
+  changeCampaignEndDate,
   deleteCampaignByCampaignName,
   deletePendingCampaigns,
 }
--- Models/campaignsModel.js
+++ Models/campaignsModel.js
@@ -8,19 +8,29 @@ const Campaign = sequelize.define("Campaign", {
       msg: "Please enter a name for the campaign"
     }
   }, unique: true},
+//---------------------------------------------------------------------------------------------------------------------------------1
   description: {type: DataTypes.STRING, allowNull: false},
+//---------------------------------------------------------------------------------------------------------------------------------2
   campaign_image: {type: DataTypes.STRING, allowNull: false},
+//---------------------------------------------------------------------------------------------------------------------------------3
   target: {type: DataTypes.INTEGER, allowNull: false}, //better for calculations
+//---------------------------------------------------------------------------------------------------------------------------------4
   creator_id: {type: DataTypes.INTEGER, allowNull: false, references: {
     model: User, key: "id" //connected the campaign to its unique creator through thr User's primary key
   }},
+//---------------------------------------------------------------------------------------------------------------------------------5
   amount: {type: DataTypes.INTEGER, allowNull: false},
+//---------------------------------------------------------------------------------------------------------------------------------6
   isAccepted: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
-  isHidden: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
+//---------------------------------------------------------------------------------------------------------------------------------7
+  isHidden: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false}, //Added for hide campaign functionality
+//---------------------------------------------------------------------------------------------------------------------------------8
   start_date: {type: DataTypes.DATEONLY, allowNull: false},
+//---------------------------------------------------------------------------------------------------------------------------------9
   end_date: {type: DataTypes.DATEONLY, allowNull: false},
 }
 )
+//relations -----------------------------------------------------------------------------------------------------------------------
 User.hasMany(Campaign, {foreignKey: "creator_id"}); //one user-to-many campaigns
 Campaign.belongsTo(User, {foreignKey: "creator_id"}); //one campaign-to-one user through creatorId
 

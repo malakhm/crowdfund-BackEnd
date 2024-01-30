fadd1c51553906872911683af69a5f055efa9b74
Rananemer08 <rananemer2233@gmail.com>
2024-01-30 22:15:28 +0200

    Update Config/cloudinary.js

--- Config/cloudinary.js
+++ Config/cloudinary.js
@@ -1,22 +1,22 @@
-import { v2 as cloudinary } from "cloudinary";
-import multer from 'multer';
-import {CloudinaryStorage} from 'multer-storage-cloudinary';
+// import { v2 as cloudinary } from "cloudinary";
+// import multer from 'multer';
+// import {CloudinaryStorage} from 'multer-storage-cloudinary';
 
-//configure
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-  secure: true,
-});
+// //configure
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET,
+//   secure: true,
+// });
 
-//storage
-const cloudinary_storage = new CloudinaryStorage ({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'fund-for-all-images', //name of folder in cloudinary cloud
-  }
-});
+// //storage
+// const cloudinary_storage = new CloudinaryStorage ({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'fund-for-all-images', //name of folder in cloudinary cloud
+//   }
+// });
 
-//multer uploader
-export const upload = multer({ storage: cloudinary_storage});
+// //multer uploader
+// export const upload = multer({ storage: cloudinary_storage});
